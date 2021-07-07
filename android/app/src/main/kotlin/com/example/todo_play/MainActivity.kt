633461b98b656761e8c6f885bb7a3cf41c0b07f2
herocodess <momohhero2@gmail.com>
2021-07-07 18:52:57 +0100

    initialized project

---
+++ android/app/src/main/kotlin/com/example/todo_play/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_play
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'screens/tasks/tasks.dart';
+
+void main() {
+  return runApp(MaterialApp(title: 'TodoList', home: Tasks()));
+}
